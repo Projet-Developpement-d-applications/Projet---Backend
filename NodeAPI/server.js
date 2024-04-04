88affff190e32f632c9e03a83768e88c73b98e7d
Alexandre Chamberland <129209077+AlexChamby@users.noreply.github.com>
2024-04-04 15:40:09 -0400

    Avancement

--- NodeAPI/server.js
+++ NodeAPI/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 const session = require("express-session");
 const cookieParser = require('cookie-parser');
@@ -13,11 +13,11 @@ app.use(cookieParser());
 
 // to change
 const db = mysql.createConnection({
-  host: "formatif5-bd-appweb2-formatif5.aivencloud.com",
-  database: "tp2db",
-  user: "user-tp2",
-  password: "AVNS_X4-y2mtSOgPvvph1Tz6",
-  port: 18715,
+  host: "mysql-bd-projet-dev-app.a.aivencloud.com",
+  database: "projetbd",
+  user: "avnadmin",
+  password: "AVNS_9EfKyeixN-RfjSoh1W1",
+  port: 21790,
 });
 
 
@@ -31,10 +31,11 @@ app.use(
   })
 );
 
+// to change
 app.use(
   session({                     // initialise la session
     key: "sessionID",              // nom du cookie
-    secret: "wakawaka",         // utilisé pour hasché l'id de session
+    secret: "projectcookie",         // utilisé pour hasché l'id de session
     resave: false,              // "L'option resave détermine si la session doit être enregistrée à nouveau à chaque fois que la page est chargée. Si resave est réglé sur false, cela signifie que la session ne sera pas sauvegardée à nouveau à moins qu'il y ait eu des modifications apportées à la session pendant la demande. Cela peut être utile pour améliorer les performances, car cela évite de sauvegarder la session à chaque requête, ce qui peut être coûteux en termes de ressources serveur." ChatGPT
     saveUninitialized: false,   // "L'option saveUninitialized détermine si une session non initialisée doit être enregistrée dans le store. Une session est considérée comme non initialisée si elle n'a pas été modifiée depuis sa création. Si saveUninitialized est réglé sur false, cela signifie que les sessions qui n'ont pas été modifiées (par exemple, en ajoutant des données à la session) ne seront pas enregistrées dans le store. Cela peut être utile pour économiser de l'espace de stockage et éviter de créer des sessions inutiles pour les utilisateurs qui visitent votre site sans effectuer d'action qui nécessite une session." ChatGPt
     cookie: {                   //
@@ -52,5 +53,6 @@ app.listen(port, () => {
 
 //Modules
 require('./modules/web_browser')(app);
-//require('./modules/users')(app, db);
-require('./modules/player')(app);
+require('./modules/utilisateur')(app, db);
+require('./modules/equipe')(app, db);
+require('./modules/joueur')(app, db);
