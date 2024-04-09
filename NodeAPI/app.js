f590bc3f978109d0c536b69ec857e6014bf2d9dc
Alexandre Chamberland <129209077+AlexChamby@users.noreply.github.com>
2024-04-09 12:23:35 -0400

    Update app.js

--- NodeAPI/app.js
+++ NodeAPI/app.js
@@ -24,7 +24,7 @@ const db = mysql.createConnection({
 // to change
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://app-web2-tp2.vercel.app", /\.nercolive\.site$/],
+    origin: ["http://localhost:3000", "https://projet-web-acac.vercel.app", /\.nercolive\.site$/],
     methods: ["GET", "POST", "HEAD"],
     credentials: true,
     httpOnly: false
