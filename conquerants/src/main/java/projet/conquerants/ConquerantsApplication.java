5938b6924d0a2a95877a9c3c2b00631523f2c0b4
Alexandre Chamberland <129209077+AlexChamby@users.noreply.github.com>
2024-05-21 14:28:07 -0400

    test timezone

--- conquerants/src/main/java/projet/conquerants/ConquerantsApplication.java
+++ conquerants/src/main/java/projet/conquerants/ConquerantsApplication.java
@@ -3,10 +3,13 @@ package projet.conquerants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ConquerantsApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Montreal"));
 		SpringApplication.run(ConquerantsApplication.class, args);
 	}
 
