d08880350d73da9d9237473efc430f9b892a06ef
Alexandre Chamberland <129209077+AlexChamby@users.noreply.github.com>
2024-05-30 10:39:22 -0400

    Update JwtService.java

--- conquerants/src/main/java/projet/conquerants/Service/JwtService.java
+++ conquerants/src/main/java/projet/conquerants/Service/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoder;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import projet.conquerants.Model.Utilisateur;
@@ -16,7 +17,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
     @Value("${secret.key}")
-    private final String SECRET_KEY;
+    private String SECRET_KEY;
 
     public String generateToken(Utilisateur user) {
         String token = Jwts
