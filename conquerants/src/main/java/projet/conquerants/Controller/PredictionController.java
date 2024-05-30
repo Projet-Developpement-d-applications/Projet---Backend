44d6aa9de32e540160129c78c4be981932c9e429
Alexandre Chamberland <129209077+AlexChamby@users.noreply.github.com>
2024-05-30 17:27:13 -0400

    Update PredictionController.java

--- conquerants/src/main/java/projet/conquerants/Controller/PredictionController.java
+++ conquerants/src/main/java/projet/conquerants/Controller/PredictionController.java
@@ -4,10 +4,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import projet.conquerants.Exception.ExisteDejaException;
 import projet.conquerants.Exception.ManqueInfoException;
 import projet.conquerants.Model.Match;
@@ -71,7 +68,7 @@ public class PredictionController {
         return response;
     }
 
-    @PostMapping("predictionParUtilisateur")
+    @GetMapping("predictionParUtilisateur")
     public List<Prediction> predictionParUtilisateur(HttpServletRequest request) {
         String token = null;
         Utilisateur utilisateur = null;
