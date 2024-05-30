efffb44c91a950ac7c0e186e2e6c0af93f8b566f
Alexandre Chamberland <129209077+AlexChamby@users.noreply.github.com>
2024-05-30 15:59:22 -0400

    Update PredictionController.java

--- conquerants/src/main/java/projet/conquerants/Controller/PredictionController.java
+++ conquerants/src/main/java/projet/conquerants/Controller/PredictionController.java
@@ -1,5 +1,7 @@
 package projet.conquerants.Controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,6 +15,7 @@ import projet.conquerants.Model.Prediction;
 import projet.conquerants.Model.Request.PredictionRequest;
 import projet.conquerants.Model.Utilisateur;
 import projet.conquerants.Service.DatabaseService;
+import projet.conquerants.Service.JwtService;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -22,32 +25,42 @@ import java.util.NoSuchElementException;
 public class PredictionController {
 
     private DatabaseService database;
+    private JwtService jwtService;
 
     @Autowired
-    public PredictionController(DatabaseService database) {
+    public PredictionController(DatabaseService database, JwtService jwtService) {
         this.database = database;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("ajouterPrediction")
-    public ResponseEntity<String> ajouterPrediction(@RequestBody PredictionRequest request) {
+    public ResponseEntity<String> ajouterPrediction(@RequestBody PredictionRequest request, HttpServletRequest servletRequest) {
         ResponseEntity<String> response = null;
 
         try {
-            Utilisateur utilisateur = database.getUtilisateur(request.getPseudo()).get();
-            Match match = database.getMatchParId(request.getId_match());
-
-            valideExistePas(match, utilisateur);
-            valideRequest(request, match, utilisateur);
-
-            if (!match.getJouer()) {
-                Prediction prediction = new Prediction(request.getVote(), match, utilisateur);
-                if (database.createPrediction(prediction) != null) {
-                    response = ResponseEntity.status(403).body("La prédiction a été créé avec succès");
-                } else {
-                    response = ResponseEntity.status(403).body("La prédiction n'a pas pu être créé");
+            String token = null;
+            if (servletRequest.getCookies() != null) {
+                for (Cookie cookie : servletRequest.getCookies()) {
+                    if (cookie.getName().equals("token")) {
+                        token = cookie.getValue();
+                    }
                 }
-            } else {
-                response = ResponseEntity.status(403).body("Le match a déjà été joué, vous ne pouvez pas faire de prédiction");
+                Utilisateur utilisateur = database.getUtilisateur(jwtService.extractUsername(token)).orElse(null);
+                Match match = database.getMatchParId(request.getId_match());
+
+                valideExistePas(match, utilisateur);
+                valideRequest(request, match, utilisateur);
+
+                if (!match.getJouer()) {
+                    Prediction prediction = new Prediction(request.getVote(), match, utilisateur);
+                    if (database.createPrediction(prediction) != null) {
+                        response = ResponseEntity.status(403).body("La prédiction a été créé avec succès");
+                    } else {
+                        response = ResponseEntity.status(403).body("La prédiction n'a pas pu être créé");
+                    }
+                } else {
+                    response = ResponseEntity.status(403).body("Le match a déjà été joué, vous ne pouvez pas faire de prédiction");
+                }
             }
         } catch (ManqueInfoException | NoSuchElementException e) {
             response = ResponseEntity.status(403).body("Les informations fournies ne sont pas valides");
@@ -59,8 +72,18 @@ public class PredictionController {
     }
 
     @PostMapping("predictionParUtilisateur")
-    public List<Prediction> predictionParUtilisateur(@RequestBody PredictionRequest request) {
-        Utilisateur utilisateur = database.getUtilisateur(request.getPseudo()).get();
+    public List<Prediction> predictionParUtilisateur(HttpServletRequest request) {
+        String token = null;
+        Utilisateur utilisateur = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("token")) {
+                    token = cookie.getValue();
+                }
+            }
+
+            utilisateur = database.getUtilisateur(jwtService.extractUsername(token)).orElse(null);
+        }
 
         return database.getPredictionParUtilisateur(utilisateur);
     }
@@ -82,5 +105,9 @@ public class PredictionController {
         if (database.getPredictionParUtilisateurEtMatch(utilisateur, match) != null) {
             throw new ExisteDejaException();
         }
+    }
+
+    private String extractUsername(String token) {
+        return jwtService.extractUsername(token);
     }
 }
